103317c92d1d1e7402ad869c48127b0ad5b816dc
Utkarsh Singh <75792507+UtkarshS007@users.noreply.github.com>
2025-05-27 01:04:43 -0400

    Changes to the Dagster File

--- churn-pipeline.py
+++ churn-pipeline.py
@@ -3,17 +3,22 @@ import subprocess
 
 @op
 def ingest_data():
-    subprocess.run(["python", "C:\Projects\Local-Churn-Analytics-Pipeline\ingestion.py"])
+    subprocess.run(["python", "C:/Projects/Local-Churn-Analytics-Pipeline/ingestion.py"])
+    return "Ingestion of Data Completed."
 
 @op
-def transform_data():
-    subprocess.run(["python", "C:\Projects\Local-Churn-Analytics-Pipeline\featureeng.py"])
+def transform_data(context, ingestion_status: str):
+    context.log.info(ingestion_status)
+    subprocess.run(["python", "C:/Projects/Local-Churn-Analytics-Pipeline/featureeng.py"])
+    return "Feature Engineering and Transformation Completed."
 
 @op
-def train_model():
-    subprocess.run(["python", "C:\Projects\Local-Churn-Analytics-Pipeline\ML\train.py"])
+def train_model(context, transformation_status: str):
+    context.log.info(transformation_status)
+    subprocess.run(["python", "C:/Projects/Local-Churn-Analytics-Pipeline/ML/train.py"])
 
 @job
 def churn_prediction_pipeline():
-    transform_data(ingest_data())
-    train_model(transform_data())
+    ingestion_status = ingest_data()
+    transformation_status = transform_data(ingestion_status)
+    train_model(transformation_status)
